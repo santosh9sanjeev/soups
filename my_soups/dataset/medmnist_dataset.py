f977838b8409eb087f11b80dc6252f5de15210ae
santosh9sanjeev <santosh9sanjeev@gmail.com>
2023-09-07 20:57:10 +0400

    commit_t1

---
+++ my_soups/dataset/medmnist_dataset.py
@@ -0,0 +1,22 @@
+import medmnist
+
+class PneumoniaDataset:
+    def __init__(self, download=True):
+        self.download = download
+
+    def get_data(self):
+        #DataClass = getattr(medmnist, info['python_class'])
+        dataset = DataClass(root='./data', split='train', download=self.download)
+
+        return dataset
+
+
+class OrganCMNIST:
+    def __init__(self, download=True):
+        self.download = download
+
+    def get_data(self):
+        DataClass = getattr(medmnist, info['python_class'])
+        #dataset = DataClass(root='./data', split='train', download=self.download)
+
+        return DataClass
